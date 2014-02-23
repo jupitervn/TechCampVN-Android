e746001cd88e88daa092855092f832cfd49d4380
Cao Duy Vu <vu.cao.duy@gmail.com>
2014-02-23 23:59:20 +0700

    [FIX] Fix class type inside GsonHandler.

--- app/src/main/java/vn/techcamp/services/GsonHttpResponseHandler.java
+++ app/src/main/java/vn/techcamp/services/GsonHttpResponseHandler.java
@@ -2,28 +2,18 @@ package vn.techcamp.services;
 
 import com.loopj.android.http.BaseJsonHttpResponseHandler;
 
-import org.apache.http.Header;
-
 import vn.techcamp.ui.JSONUtils;
 import vn.techcamp.ui.MiscUtils;
+
 
 /**
  * Created by jupiter on 22/2/14.
  */
-public class GsonHttpResponseHandler<T> extends BaseJsonHttpResponseHandler<T> {
+public abstract class GsonHttpResponseHandler<T> extends BaseJsonHttpResponseHandler<T> {
     private Class<T> clazz;
+
     public GsonHttpResponseHandler(Class<T> clazz) {
         this.clazz = clazz;
-    }
-
-    @Override
-    public void onSuccess(int i, Header[] headers, String s, T t) {
-
-    }
-
-    @Override
-    public void onFailure(int i, Header[] headers, Throwable throwable, String s, T t) {
-
     }
 
     @Override
--- app/src/main/java/vn/techcamp/services/HttpService.java
+++ app/src/main/java/vn/techcamp/services/HttpService.java
@@ -1,26 +1,26 @@
 /**
-*/
+ */
 package vn.techcamp.services;
 
+
 import com.loopj.android.http.AsyncHttpClient;
-import com.loopj.android.http.BaseJsonHttpResponseHandler;
 
 import org.apache.http.Header;
 
 import vn.techcamp.models.Topic;
 
+
 /**
  * @author Cao Duy Vu (vu.caod@skunkworks.vn)
- *
  */
 public class HttpService {
-    private static AsyncHttpClient httpClient = new AsyncHttpClient();
     private static final String BASE_URL = "http://techcamp.vn/";
-    private static final String TOPICS_PATH = BASE_URL + "voting/topic/api";
     private static final String VOTE_PATH = BASE_URL + "voting/topic/vote";
+    private static final String TOPICS_PATH = BASE_URL + "voting/topic/api";
+    private static AsyncHttpClient httpClient = new AsyncHttpClient();
 
     public static void getTopics(String deviceId) {
-        httpClient.get(TOPICS_PATH,  new BaseJsonHttpResponseHandler<Topic[]>() {
+        httpClient.get(TOPICS_PATH, new GsonHttpResponseHandler<Topic[]>(Topic[].class) {
             @Override
             public void onSuccess(int statusCode, Header[] headers, String s, Topic[] topics) {
 
@@ -31,11 +31,8 @@ public class HttpService {
 
             }
 
-            @Override
-            protected Topic[] parseResponse(String s) throws Throwable {
-                return new Topic[0];
-            }
         });
     }
 
 }
+
